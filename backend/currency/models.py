982284f2ca787865a3cd40dfd39f0550141d199d
Bartosz Baran <bartosz.baran192@gmail.com>
2023-05-22 22:14:56 +0200

    replace relations between models

--- backend/currency/models.py
+++ backend/currency/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class CurrencyName(models.Model):
     name = models.CharField(max_length=35)
     code = models.CharField(max_length=3)
-    currency_values = models.ManyToManyField("CurrencyValue", related_name="currency_values")
 
     def __str__(self):
         return self.code
@@ -15,8 +14,7 @@ class CurrencyName(models.Model):
 
 
 class CurrencyDate(models.Model):
-    date = models.DateField(db_index=True)
-    currency_names = models.ManyToManyField("CurrencyName", related_name="currency_dates")
+    date = models.DateField(db_index=True, unique=True)
 
     def __str__(self):
         return str(self.date)
@@ -28,6 +26,8 @@ class CurrencyDate(models.Model):
 
 class CurrencyValue(models.Model):
     exchange_rate = models.DecimalField(max_digits=10, decimal_places=8)
+    currency_names = models.ManyToManyField("CurrencyName", related_name="currency_values")
+    currency_dates = models.ManyToManyField("CurrencyDate", related_name="currency_values")
 
     def __str__(self):
         return str(self.exchange_rate)
