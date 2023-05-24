71f6058306eeadbf36f9fe7b351019f79c19a3e3
Bartosz Baran <bartosz.baran192@gmail.com>
2023-05-24 16:42:03 +0200

    conftest for model tests

---
+++ backend/conftest.py
@@ -0,0 +1,25 @@
+from datetime import date, timedelta
+
+import pytest
+
+from currency.models import CurrencyName, CurrencyDate, CurrencyValue
+
+
+@pytest.fixture
+def currency(db):
+    currency_name = CurrencyName.objects.get(code="USD")
+    currency_date = CurrencyDate.objects.create(date=date.today())
+    currency_value = CurrencyValue.objects.create(exchange_rate=0.10)
+    currency_value.currency_names.add(currency_name)
+    currency_value.currency_dates.add(currency_date)
+    return currency_date
+
+
+@pytest.fixture
+def currency_2(db):
+    currency_name = CurrencyName.objects.get(code="USD")
+    currency_date = CurrencyDate.objects.create(date=date.today() - timedelta(days=2))
+    currency_value = CurrencyValue.objects.create(exchange_rate=0.20)
+    currency_value.currency_names.add(currency_name)
+    currency_value.currency_dates.add(currency_date)
+    return currency_date
